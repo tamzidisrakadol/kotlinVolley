57eed68f8fba349444bfdb8ff6f33bd526175459
Tamzid Israk <tamzidisrak12@gmail.com>
2023-04-26 03:47:21 +0600

    fetch data from mySql database add swipe refresh layout and shimmer_layout.xml

--- app/src/main/java/com/example/datafetchfromphp/views/MainActivity.kt
+++ app/src/main/java/com/example/datafetchfromphp/views/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.datafetchfromphp.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
@@ -13,6 +15,10 @@ import com.example.datafetchfromphp.adapter.CustomerAdapter
 import com.example.datafetchfromphp.databinding.ActivityMainBinding
 import com.example.datafetchfromphp.model.CustomerModel
 import com.example.datafetchfromphp.utility.Constraints
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.json.JSONArray
 import org.json.JSONException
 
@@ -25,14 +31,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        showCustomerList()
+
+        //start effect + loading data
+        showShimmerEffect()
+
+        //recyclerView layout manager
         binding.recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL,false)
+
+
+
+        //swipe Refresh layout
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            binding.recyclerView.visibility = View.INVISIBLE
+            binding.shimmerFrameLayout.visibility = View.VISIBLE
+            showShimmerEffect()
+            binding.swipeRefreshLayout.isRefreshing = false
+        }
+
+
     }
 
-    private fun showCustomerList(){
+    override fun onResume() {
+        super.onResume()
+        fetchCustomerList()
+    }
+
+    //collect data from mySql
+    private fun fetchCustomerList(){
         val stringRequest = StringRequest(Request.Method.POST,Constraints.customerURl, {
             try {
                 val jsonArray = JSONArray(it)
+                customerList.clear()
                 for (i in 0 until jsonArray.length()){
                     val jsonObject = jsonArray.getJSONObject(i)
                     val customerModel = CustomerModel()
@@ -44,6 +73,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 val customerAdapter = CustomerAdapter(customerList)
                 binding.recyclerView.adapter = customerAdapter
+                customerAdapter.notifyDataSetChanged()
 
             }catch (e:JSONException){
                Log.d("eTag",e.toString())
@@ -53,6 +83,21 @@ class MainActivity : AppCompatActivity() {
         })
         val requestQueue: RequestQueue = Volley.newRequestQueue(this)
         requestQueue.add(stringRequest)
+    }
+
+
+    private fun showShimmerEffect(){
+        lifecycleScope.launch {
+            binding.shimmerFrameLayout.startShimmer()
+            delay(3000)
+            binding.shimmerFrameLayout.stopShimmer()
+            binding.shimmerFrameLayout.visibility = View.INVISIBLE
+            withContext(Dispatchers.Main){
+                fetchCustomerList()
+                binding.recyclerView.visibility= View.VISIBLE
+            }
+
+        }
     }
 
 }
